0c2c2a78633fc0039d0fc7d5d9ab24227ccacd49
nnbko <dadaya999@yandex.ru>
2024-02-20 22:36:15 +0300

    deploy3

--- utils/validation.js
+++ utils/validation.js
@@ -5,7 +5,7 @@ const regex = /^https?:\/\/(www\.)?[a-zA-Z0-9]+([.|-]{1}[a-zA-Z0-9]+)*\.[a-zA-Z0
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
   }),
 });
